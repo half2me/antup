3cb0b683094a00bc32ed5ee6a96371b1e5277162
Tamási Benjamin <halftome@yahoo.com>
2016-04-26 12:35:05 +0200

    patch

--- antup.py
+++ antup.py
@@ -15,11 +15,12 @@ from ant.core.constants import *
 from config import *
 
 # ZMQ
-zcontext = zmq.Context()
-socket = zcontext.socket(zmq.REQ)
-socket.connect ("tcp://middle-layer-ip:9999")
+#zcontext = zmq.Context()
+#socket = zcontext.socket(zmq.REQ)
+#socket.connect ("tcp://middle-layer-ip:9999")
 
 NETKEY = '\xB9\xA5\x21\xFB\xBD\x72\xC3\x45'
+SERIAL = '/dev/tty.serial1'
 
 
 # A run-the-mill event listener
@@ -27,12 +28,12 @@ class HRMListener(event.EventCallback):
     def process(self, msg):
         if isinstance(msg, message.ChannelBroadcastDataMessage):
             print 'Heart Rate:', ord(msg.payload[-1])
-            socket.send(ord(msg.payload[-1])) # Change this to JSON data
+            #socket.send(ord(msg.payload[-1])) # Change this to JSON data
             # Add lock to method since sockets are not thread-safe
 
 
 # Initialize
-stick = driver.USB1Driver(SERIAL, log=LOG, debug=DEBUG)
+stick = driver.USB1Driver(SERIAL, log=None, debug=None)
 antnode = node.Node(stick)
 antnode.start()
 
