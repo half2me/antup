3c8e0077ace1a8660f969bc40ee8ba638e56513d
Tamási Benjamin <halftome@yahoo.com>
2016-04-27 17:31:59 +0200

    patch

--- antup.py
+++ antup.py
@@ -4,7 +4,7 @@ Connect to AMT+ devices and upload data to a server via ZMQ
 
 import sys
 import time
-import zmq
+#import zmq
 
 from ant.core import driver
 from ant.core import node
@@ -20,39 +20,39 @@ from config import *
 #socket.connect ("tcp://middle-layer-ip:9999")
 
 NETKEY = '\xB9\xA5\x21\xFB\xBD\x72\xC3\x45'
-SERIAL = '/dev/tty.serial1'
 
 
 # A run-the-mill event listener
-class HRMListener(event.EventCallback):
+class Listener(event.EventCallback):
     def process(self, msg):
         if isinstance(msg, message.ChannelBroadcastDataMessage):
-            print 'Heart Rate:', ord(msg.payload[-1])
+            #print 'Speed:', ord(msg.payload[-1])
+            print 'yolo'
             #socket.send(ord(msg.payload[-1])) # Change this to JSON data
             # Add lock to method since sockets are not thread-safe
 
 
 # Initialize
-stick = driver.USB1Driver(SERIAL, log=None, debug=None)
+stick = driver.USB2Driver(idProduct=0x1009)
 antnode = node.Node(stick)
 antnode.start()
 
 # Setup channel
-key = node.NetworkKey('N:ANT+', NETKEY)
-antnode.setNetworkKey(0, key)
+network = node.Network(NETKEY, 'N:ANT+')
+antnode.setNetworkKey(0, network)
 channel = antnode.getFreeChannel()
-channel.name = 'C:HRM'
-channel.assign('N:ANT+', CHANNEL_TYPE_TWOWAY_RECEIVE)
-channel.setID(120, 0, 0)
-channel.setSearchTimeout(TIMEOUT_NEVER)
-channel.setPeriod(8070)
-channel.setFrequency(57)
+channel.name = 'C:Speed'
+channel.assign(network, CHANNEL_TYPE_TWOWAY_RECEIVE)
+channel.setID(123, 0, 0)
+channel.searchTimeout = TIMEOUT_NEVER
+channel.period = 8118
+channel.frequency = 57
 channel.open()
 
 # Setup callback
 # Note: We could also register an event listener for non-channel events by
 # calling registerEventListener() on antnode rather than channel.
-channel.registerCallback(HRMListener())
+channel.registerCallback(Listener())
 
 # Wait
 print "Listening for HR monitor events (120 seconds)..."
