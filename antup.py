e9bc3f4fd91deaceeb9d168f97d9f008654f0c23
Tamási Benjamin <h@lfto.me>
2016-05-25 23:01:30 +0200

    Servo, ErrorHandling, Options (#2)
    * Accept incomming WS events and better error handling
    
    * Python3 compatibility
    
    * Added start params

--- antup.py
+++ antup.py
@@ -4,11 +4,8 @@
 Connect to AMT+ devices and upload data to a server via WebSocket
 """
 from __future__ import print_function
-
 import sys
-import time
-
-from websocket import create_connection
+from websocket import create_connection, socket, WebSocketConnectionClosedException
 
 from ant.core import driver
 from ant.core import node
@@ -19,11 +16,18 @@ from ant.core.constants import *
 from profiles.PowerMessage import PowerMessage
 from profiles.SpeedCadenceMessage import SpeedCadenceMessage
 
-NETKEY = '\xB9\xA5\x21\xFB\xBD\x72\xC3\x45'
+NETKEY = b'\xB9\xA5\x21\xFB\xBD\x72\xC3\x45'
+
+id = 1
+bikeId = 0
+
+if len(sys.argv) >= 2:
+    id = sys.argv[1]
+if len(sys.argv) >= 3:
+    bikeId = sys.argv[2]
 
 # Callback for ANT+ events
 class Listener(event.EventCallback):
-
     def __init__(self):
         self.previousMessageSpeedCadence = None
         self.previousMessagePower = None
@@ -34,18 +38,21 @@ class Listener(event.EventCallback):
             if channel.name == "speedcadence":
                 decoded = SpeedCadenceMessage(self.previousMessageSpeedCadence, msg.payload)
 
-                print("Speed: %f" % decoded.speed(2096))
-                print("Cadence: %f" % decoded.cadence)
-                print("")
-                ws.send('{"cmd":"bike-update", "speed":' + str(decoded.speed(2096)) + ', "cadence":' + str(decoded.cadence) + '}')
+                # print("Speed: %f" % decoded.speed(2096))
+                # print("Cadence: %f" % decoded.cadence)
+                # print("")
+                print(".", endl="")
+                ws.send('{"cmd":"bike-update", "speed":' + str(decoded.speed(2096)) + ', "cadence":' + str(
+                    decoded.cadence) + '}')
                 self.previousMessageSpeedCadence = decoded
 
             # Power
             if channel.name == "power":
-                if msg.payload[1] == 0x10: # Standard Power Only!
+                if msg.payload[1] == 0x10:  # Standard Power Only!
                     decoded = PowerMessage(self.previousMessagePower, msg.payload)
-                    print("Power: %f" % decoded.averagePower)
-                    print("")
+                    # print("Power: %f" % decoded.averagePower)
+                    # print("")
+                    print(".", endl="")
                     ws.send('{"cmd":"bike-update", "power":' + str(decoded.averagePower) + '}')
                     self.previousMessagePower = None
 
@@ -62,7 +69,7 @@ antnode.setNetworkKey(0, network)
 channel1 = antnode.getFreeChannel()
 channel1.name = "speedcadence"
 channel1.assign(network, CHANNEL_TYPE_TWOWAY_RECEIVE)
-channel1.setID(121, 0, 0)
+channel1.setID(121, int(bikeId), 0)
 channel1.searchTimeout = TIMEOUT_NEVER
 channel1.period = 8086
 channel1.frequency = 57
@@ -73,24 +80,33 @@ channel1.registerCallback(Listener())
 channel2 = antnode.getFreeChannel()
 channel2.name = 'power'
 channel2.assign(network, CHANNEL_TYPE_TWOWAY_RECEIVE)
-channel2.setID(11, 0, 0)
+channel2.setID(11, int(bikeId), 0)
 channel2.searchTimeout = TIMEOUT_NEVER
 channel2.period = 8182
 channel2.frequency = 57
 channel2.open()
 channel2.registerCallback(Listener())
 
-# Web Socket Magic
-ws = create_connection("ws://127.0.0.1:8080")
-ws.send(u'{"cmd":"bike-register", "id":1}'.encode('utf8'))
+ws = None
+try:
+    # Web Socket Magic
+    ws = create_connection("ws://127.0.0.1:8080")
+    ws.send('{"cmd":"bike-register", "id":' + str(id) + '}')
 
-# Wait
-time.sleep(60*60)
-
-# Shutdown
-channel1.close()
-channel1.unassign()
-channel2.close()
-channel2.unassign()
-antnode.stop()
-ws.close()
+    while True:
+        recv = ws.recv()
+        print(recv)
+except socket.error as err:
+    print("Disconnected!")
+except WebSocketConnectionClosedException:
+    print("Disconnected!")
+except KeyboardInterrupt:
+    print("Exiting...")
+finally:
+    if ws is not None:
+        ws.close()
+    channel1.close()
+    channel1.unassign()
+    channel2.close()
+    channel2.unassign()
+    antnode.stop()
