d5958d283a962f4ed7f8c1640d507479c5aa4902
Tamási Benjamin <halftome@yahoo.com>
2016-06-01 13:15:03 +0200

    sleepy fakeANT

--- fakeant.py
+++ fakeant.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
-from websocket import create_connection
+from websocket import create_connection, socket, WebSocketConnectionClosedException
 import sys
+import time
 
 if len(sys.argv) == 5:
     id = sys.argv[1]
@@ -12,12 +13,25 @@ else:
     print("Usage: " + str(sys.argv[0] + " id speed cadence power"))
     exit()
 
-# Web Socket Magic
-ws = create_connection("ws://127.0.0.1:8080")
-ws.send('{"cmd":"bike-register", "id":' + str(id) + '}')
+ws = None
+try:
 
-while True:
-    ws.send('{"cmd":"bike-update", "speed":' + str(speed) + ', "cadence":' + str(cadence) + '}')
-    ws.send('{"cmd":"bike-update", "power":' + str(power) + '}')
+    # Web Socket Magic
+    ws = create_connection("ws://127.0.0.1:8080")
+    ws.send('{"cmd":"bike-register", "id":' + str(id) + '}')
 
-ws.close()
+    while True:
+        ws.send('{"cmd":"bike-update", "speed":' + str(speed) + ', "cadence":' + str(cadence) + '}')
+        ws.send('{"cmd":"bike-update", "power":' + str(power) + '}')
+        time.sleep(1)
+
+
+except socket.error as err:
+    print("Disconnected!")
+except WebSocketConnectionClosedException:
+    print("Disconnected!")
+except KeyboardInterrupt:
+    print("Exiting...")
+finally:
+    if ws is not None:
+        ws.close()
